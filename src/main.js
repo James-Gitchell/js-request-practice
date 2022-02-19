6f8968df6f88a421c45d5948806b79aa53d455a3
James Gitchell <jamesgitchell.dev@gmail.com>
2022-02-19 18:08:13 -0500

    attempted tests first

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5000";
 
-function getAllNames() {}
+function getAllNames() {
+  axios.get(`${BASE_URL}/constellations`)
+      .then(Response=> {
+        let constellationNames
+         = Response.data.map(constellation=> constellation.name)
+         console.log(constellationNames)
+         .catch(err=> console.log(err))
 
-function getConstellationsByQuadrant(quadrant) {}
+      })
+}
+
+function getConstellationsByQuadrant(quadrant) {
+        axios.get(`${BASE_URL}/constellations`)
+               .then(Response=> {
+                 const constellations = Response.data.filter(stars=> stars.quadrant === quadrant)
+                 console.log(constellations)
+                   .catch(error=> console.log(error))
+               })
+}
 
 module.exports = {
   getAllNames,
